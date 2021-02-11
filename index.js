67df9d6529a11fcefab3918fa4632d2fc17b02e7
David Smith <dasmith2951@gmail.com>
2021-02-10 20:53:21 -0500

    Task 4 complete
    originally I had two arrays both named inning instead of creating an inning callback array, once I fixed that the concept executed perfectly.

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(inning, inningsPlayed) {
+function finalScore(inningCallback, inningsPlayed) {
 
   let finalScore = {
     Home: 0,
@@ -98,8 +98,8 @@ function finalScore(inning, inningsPlayed) {
 
   for (let i = 0; i <= inningsPlayed; i++) {
 
-    let home = inning();
-    let away = inning();
+    let home = inningCallback(i);
+    let away = inningCallback(i);
 
     finalScore.Home = finalScore.Home + home;
     finalScore.Away = finalScore.Away + away;
@@ -117,12 +117,20 @@ Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(inningScore) {
+  
+  let score = {
+
+    Home: inningScore(9),
+    Away: inningScore(9)
+
+  };
+
+  return score;
+
 }
 
-
-//look @ array.map() for task 5 as a possibility.
+console.log(getInningScore(inning));
 
 /* ⚾️⚾️⚾️ Task 5: scoreboard() ⚾️⚾️⚾️
 Use the scoreboard function below to do the following:
